9ff4e804740c0578bc301c02653aa0baa2f02c00
Edward <eaakwasi@yahoo.ca>
2020-10-29 17:48:20 -0600

    feat: Add Python files

---
+++ PythonForDatascience/Exercises.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello\nWorld!")
+
+
+# In[2]:
+
+
+# print('Hello World!')
+
+
+# In[3]:
+
+
+int(1.0)
+
+
+# In[4]:
+
+
+bool(1)
+
+
+# In[5]:
+
+
+"0123456".find('1')
+
+
+# In[6]:
+
+
+3 + 2 * 2
+
+
+# In[ ]:
+
+
+
+
